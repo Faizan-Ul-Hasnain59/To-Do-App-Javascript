c8b380ada6a3fd1df888f14379eb84fee0bc87e7
faizan ul hasnain59 <faizanulhasnain188@gmail.com>
2023-07-18 19:39:32 -0700

    second_commment

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputbox = document.getElementById("input-box");
+const displayError = document.getElementById("message");
+const listcontainer = document.getElementById("list-container");
 
 function addTask() {
-    if (inputBox.value === '') {
-        alert("you must write something");
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-    }
+  if (inputbox.value === "") {
+    displayError.innerHTML = "You must write something";
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.append(span);
+    inputbox.value = "";
+    saveData(); 
+  }
 }
+
+function saveData() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showTask() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+listcontainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData(); 
+  }
+});
+
+showTask(); 
